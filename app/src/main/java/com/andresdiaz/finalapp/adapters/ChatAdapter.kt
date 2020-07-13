004255d64eafc7def4387320a60ee52dfd6bfc58
Andres <serdna.alejandro@gmail.com>
2020-07-13 11:46:24 -0500

    ChatRealTime_&_Optimizaciones

--- app/src/main/java/com/andresdiaz/finalapp/adapters/ChatAdapter.kt
+++ app/src/main/java/com/andresdiaz/finalapp/adapters/ChatAdapter.kt
@@ -24,21 +24,21 @@ class ChatAdapter(val items:List<Message>, val userId: String ): RecyclerView.Ad
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when(viewType){
-            GLOBAL_MESSAGE-> ViewHolderR(parent.inflate(layoutRight))
+            MY_MESSAGE-> ViewHolderR(parent.inflate(layoutRight))
             else ->ViewHolderL(parent.inflate(layoutLeft))
         }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when(holder.itemViewType){
-            MY_MESSAGE-> (holder as ViewHolderR).bind(items[position])
-            GLOBAL_MESSAGE->(holder as ViewHolderL).bind((items[position]))
+            MY_MESSAGE -> (holder as ViewHolderR).bind(items[position])
+            GLOBAL_MESSAGE -> (holder as ViewHolderL).bind(items[position])
         }
     }
 
     class ViewHolderR(itemView: View):RecyclerView.ViewHolder(itemView){
         fun bind(message: Message)=with(itemView){
-            textViewTimeRight.text= message.message
+            textViewMessageRight.text= message.message
             textViewTimeRight.text=SimpleDateFormat("hh:mm").format(message.sentAt)
             Picasso.get().load(message.profileImageURL).resize(100,100)
                 .centerCrop().transform(CircleTransform()).into(imageViewProfileRight)
@@ -46,7 +46,7 @@ class ChatAdapter(val items:List<Message>, val userId: String ): RecyclerView.Ad
     }
     class ViewHolderL(itemView: View):RecyclerView.ViewHolder(itemView){
         fun bind(message: Message)=with(itemView){
-            textViewTimeLeft.text= message.message
+            textViewMessageLeft.text= message.message
             textViewTimeLeft.text=SimpleDateFormat("hh:mm").format(message.sentAt)
             Picasso.get().load(message.profileImageURL).resize(100,100)
                 .centerCrop().transform(CircleTransform()).into(imageViewProfileLeft)
--- app/src/main/java/com/andresdiaz/finalapp/models/Message.kt
+++ app/src/main/java/com/andresdiaz/finalapp/models/Message.kt
@@ -2,4 +2,7 @@ package com.andresdiaz.finalapp.models
 
 import java.util.*
 
-data class Message(val authorId: String, val message: String, val profileImageURL: String, val sentAt: Date)
+data class Message(val authorId: String="",
+                   val message: String="",
+                   val profileImageURL: String="",
+                   val sentAt: Date=Date())
