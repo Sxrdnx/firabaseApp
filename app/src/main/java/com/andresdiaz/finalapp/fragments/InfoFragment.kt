dbf748a11c474d6f9e0d17384a0a9dcdf132c65c
Andres <serdna.alejandro@gmail.com>
2020-07-13 14:30:45 -0500

    Layout_InfoFragment

--- app/src/main/java/com/andresdiaz/finalapp/fragments/InfoFragment.kt
+++ app/src/main/java/com/andresdiaz/finalapp/fragments/InfoFragment.kt
@@ -8,12 +8,13 @@ import android.view.ViewGroup
 import com.andresdiaz.finalapp.R
 
 class InfoFragment : Fragment() {
-
+    private lateinit var _View:View
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false)
+        _View=inflater.inflate(R.layout.fragment_info, container, false)
+        return _View
     }
 }
