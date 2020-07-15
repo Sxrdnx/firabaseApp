744048536a55823eeec48f105cbd0e186278c128
Andres <serdna.alejandro@gmail.com>
2020-07-14 22:36:53 -0500

    SignOut

--- app/src/main/java/com/andresdiaz/finalapp/MainActivity.kt
+++ app/src/main/java/com/andresdiaz/finalapp/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.andresdiaz.finalapp
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 import androidx.viewpager.widget.ViewPager
+import com.andresdiaz.finalapp.activities.LoginActivity
 import com.andresdiaz.finalapp.adapters.PagerAdapter
 import com.andresdiaz.finalapp.fragments.ChatFragment
 import com.andresdiaz.finalapp.fragments.InfoFragment
 import com.andresdiaz.finalapp.fragments.RatesFragment
 import com.andresdiaz.mylibrary.ToolbarActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity :ToolbarActivity() {
@@ -68,5 +72,22 @@ class MainActivity :ToolbarActivity() {
             }
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.general_options_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.menu_log_out->{
+                FirebaseAuth.getInstance().signOut()
+                goToActivity<LoginActivity>{
+                    flags= Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
